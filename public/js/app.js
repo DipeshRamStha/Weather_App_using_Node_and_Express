dca6087e3cfe4328b00881d79552d2df81af85be
DipeshRamStha <visit.dipesh933@gmail.com>
2024-04-17 08:22:03 +0545

    Resolved the Weather App heading to display above the article by using position relative

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+var weatherApi = "/weather";
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const weatherIcon = document.querySelector(".weatherIcon i");
+const weatherCondition = document.querySelector(".weatherCondition");
+const tempElement = document.querySelectory(".temperature span");
+
+const locationElement = document.querySelector(".place");
+const dateElement = document.querySelector(".date");
