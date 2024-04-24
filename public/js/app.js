c993f793872497ecb82f4e0f356a99651ad5a1ae
DipeshRamStha <visit.dipesh933@gmail.com>
2024-04-24 06:28:43 +0545

    Added the month in words and the date with getDate function

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,13 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const weatherIcon = document.querySelector(".weatherIcon i");
 const weatherCondition = document.querySelector(".weatherCondition");
-const tempElement = document.querySelectory(".temperature span");
+const tempElement = document.querySelector(".temperature span");
 
 const locationElement = document.querySelector(".place");
 const dateElement = document.querySelector(".date");
 
 const currentDate = new Date();
+const options = { month: "long" };
+const monthName = currentDate.toLocaleString("en-US", options);
 
-dateElement.textContent = currentDate;
+dateElement.textContent = currentDate.getDate() + ", " + monthName;
